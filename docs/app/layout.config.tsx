f3d682d6130f8d67a85b252ad7bdf4818ce6a084
Kinfe123 <kinfishtech@gmail.com>
2025-03-01 10:20:23 +0300

    docs and unnecessary imports

--- docs/app/layout.config.tsx
+++ docs/app/layout.config.tsx
@@ -1,33 +1,33 @@
 import { source, changelogs } from "@/app/source";
 import {
-	DocsNavbarMobileBtn,
-	DocsNavbarMobileTitle,
+  DocsNavbarMobileBtn,
+  DocsNavbarMobileTitle,
 } from "@/components/nav-mobile";
 import { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export const baseOptions: BaseLayoutProps = {
-	nav: {
-		component: (
-			<div className="flex items-center justify-between py-4 px-2.5 md:hidden">
-				<DocsNavbarMobileTitle />
-				<DocsNavbarMobileBtn />
-			</div>
-		),
-	},
-	links: [
-		{
-			text: "Documentation",
-			url: "/docs",
-			active: "nested-url",
-		},
-	],
+  nav: {
+    component: (
+      <div className="flex items-center justify-between py-4 px-2.5 md:hidden">
+        <DocsNavbarMobileTitle />
+        <DocsNavbarMobileBtn />
+      </div>
+    ),
+  },
+  links: [
+    {
+      text: "Documentation",
+      url: "/docs",
+      active: "nested-url",
+    },
+  ],
 };
 
 export const docsOptions = {
-	...baseOptions,
-	tree: source.pageTree,
+  ...baseOptions,
+  tree: source.pageTree,
 };
 export const changelogOptions = {
-	...baseOptions,
-	tree: changelogs.pageTree,
+  ...baseOptions,
+  tree: changelogs.pageTree,
 };
